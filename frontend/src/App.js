ace68573c077fadd69e7a23ca071eb28380b5062
Sourav Seal <sourav.seal1999@gmail.com>
2021-04-18 13:12:19 +0530

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const App = ()=>{
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Proshop</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
