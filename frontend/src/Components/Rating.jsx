79d46105d6954d8067c96d1c4679672480bb287f
Sourav Seal <sourav.seal1999@gmail.com>
2021-05-05 23:27:14 +0530

    added rating component

---
+++ frontend/src/Components/Rating.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Rating = ({value, reviewCount}) => {
+    return (
+        <div>
+            <div style = {{ color: "yellowgreen" }}>
+                <span>
+                    <i className = { value >= 1 ? "fas fa-star" : value >= 0.5? "fas fa-star-half-alt" :"far fa-star"} />
+                    <i className = { value >= 2 ? "fas fa-star" : value >= 1.5? "fas fa-star-half-alt" :"far fa-star"} />
+                    <i className = { value >= 3 ? "fas fa-star" : value >= 2.5? "fas fa-star-half-alt" :"far fa-star"} />
+                    <i className = { value >= 4 ? "fas fa-star" : value >= 3.5? "fas fa-star-half-alt" :"far fa-star"} />
+                    <i className = { value >= 5 ? "fas fa-star" : value >= 4.5? "fas fa-star-half-alt" :"far fa-star"} />
+                </span>
+                <div>
+                    <strong style = {{fontSize: "medium", fontFamily: "monospace"}}>
+                        {reviewCount} Reviews 
+                    </strong>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Rating
