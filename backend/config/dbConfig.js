ef5b9b72b21866f2b5f07f41b2dd0f1d5a42b3be
Sourav Seal <sourav.seal1999@gmail.com>
2022-01-13 21:06:49 +0530

    fixes: mongoDB console error caused by useCreateIndex

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
     try {
-        const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.bold)
     } catch (error) {
         console.log(error.message.red.underline.bold)
