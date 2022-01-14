a81118a869618c25ac4a64929e077721b6d32671
Sourav Seal <sourav.seal1999@gmail.com>
2022-01-14 17:22:54 +0530

    fixes: blank page on loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Homepage from './Screens/Homepage'
-const App = ()=>{
+const App = () => {
   return (
     <>
       <Homepage />
+      {/* <h1>hello</h1> */}
     </>
   );
 }
